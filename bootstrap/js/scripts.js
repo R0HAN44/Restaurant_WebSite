b731909cb4e011dcc3232aabe2216d8f46fa7c1a
R0HAN44 <rohanj0306@gmail.com>
2022-08-25 11:59:46 +0530

    bootstrap

---
+++ bootstrap/js/scripts.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+            $("#carouselbutton").click(function () {
+                if ($("#carouselbutton").children("span").hasClass('fa-pause')) {
+                    $("#mycarousel").carousel('pause');
+                    $("#carouselbutton").children("span").removeClass('fa-pause');
+                    $("#carouselbutton").children("span").addClass('fa-play');
+                }
+                else if ($("#carouselbutton").children("span").hasClass('fa-play')) {
+                    $("#mycarousel").carousel('cycle');
+                    $("#carouselbutton").children("span").removeClass('fa-play');
+                    $("#carouselbutton").children("span").addClass('fa-pause');
+                }
+            });
+            $('#reservebutton').click(function(){
+		        $('#reservemodal').modal('show');
+	        });
+	        $('#loginbutton').click(function(){
+		        $('#loginmodal').modal('show');
+	        });
+});
